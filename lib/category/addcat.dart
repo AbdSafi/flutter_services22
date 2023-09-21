746fdd2f1ee598a90d2dda4245363ebb8b709764
AbdSafi <gacours23@gmail.com>
2023-09-21 21:43:15 +0100

    flutter app - add categories commit

--- lib/category/addcat.dart
+++ lib/category/addcat.dart
@@ -8,7 +8,7 @@ class AddCategories extends StatefulWidget {
   State<AddCategories> createState() => _AddCategoriesState();
 }
 
-///
+//k
 class _AddCategoriesState extends State<AddCategories> {
   TextEditingController catController = TextEditingController();
   CollectionReference cate =
