ea0352d6bb0ecb097852988fe04f3ae584f9102d
AbdSafi <gacours23@gmail.com>
2023-09-22 14:59:39 +0100

    flutter app - add categories commit

--- lib/category/addcat.dart
+++ lib/category/addcat.dart
@@ -8,13 +8,13 @@ class AddCategories extends StatefulWidget {
   State<AddCategories> createState() => _AddCategoriesState();
 }
 
-//
 class _AddCategoriesState extends State<AddCategories> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   TextEditingController catController = TextEditingController();
   CollectionReference cate =
       FirebaseFirestore.instance.collection("categories");
 
-  Future<void> addCat() {
+  addCat() {
     return cate
         .add({
           'name': catController.text,
@@ -23,40 +23,61 @@ class _AddCategoriesState extends State<AddCategories> {
         .catchError((error) => print("Failed to add Category: $error"));
   }
 
+  addCategory() async {
+    CollectionReference cat =
+        FirebaseFirestore.instance.collection("categories");
+    if (formKey.currentState!.validate()) {
+      try {
+        cat.add({"name": catController.text});
+      } catch (e) {
+        print('$e');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: catController,
-              decoration: const InputDecoration(
-                hintText: "Enter your category",
-                prefixIcon: Icon(Icons.category),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: addCat,
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(10),
-                  elevation: 5, // Elevation (shadow)
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "cant be empty";
+                  }
+                  return null;
+                },
+                controller: catController,
+                decoration: const InputDecoration(
+                  hintText: "Enter your category",
+                  prefixIcon: Icon(Icons.category),
+                  border: OutlineInputBorder(),
                 ),
-                child: const Text('Add Category'),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: addCategory,
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    elevation: 5, // Elevation (shadow)
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: const Text('Add Category'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
