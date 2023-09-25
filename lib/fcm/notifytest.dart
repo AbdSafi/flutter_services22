1c83d68341f0adcadb4ee8a491ec0a33713325df
AbdSafi <gacours23@gmail.com>
2023-09-25 22:03:18 +0100

    flutter app - fcm commit

--- lib/fcm/notifytest.dart
+++ lib/fcm/notifytest.dart
@@ -14,6 +14,7 @@ class _NotificationFCMState extends State<NotificationFCM> {
     print('---------------------------------------');
     print(token);
   }
+  //
 
   @override
   void initState() {
